cdb64f5ff9fc00555b60515224365ee8a2cffe25
tijanaivanoviic <tijanaivanovic616@gmail.com>
2023-01-20 19:31:11 +0100

    Dodata kontakt strana

---
+++ src/pages/Kontakt.js
@@ -0,0 +1,14 @@
+import Forma from "../components/Forma"
+import './Kontakt.css'
+function Kontakt(){
+    return (
+       <div className="contact">
+        <h2 style={{"text-align": "center"}}>Pišite nam</h2>
+         <div className="wrap">
+            <Forma/> 
+            </div>  
+       </div>
+    )
+
+}
+export default Kontakt
